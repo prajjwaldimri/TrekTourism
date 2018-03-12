2074867fcf40a113bf6f964d34f500f03df86ddc
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-03-12 18:55:52 +0530

    Added options for multiple price

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,8 @@ var myLazyLoad = new LazyLoad();
 
 let trekDetails = document.getElementById('trek-details');
 let trekSelect = document.getElementById('trek-select');
-let trekPrice = document.getElementById('trek-price');
+let trekPriceFirst = document.getElementById('trek-price-first');
+let trekPriceSecond = document.getElementById('trek-price-second');
 let trekPersons = document.getElementById('trek-persons');
 
 trekSelect.addEventListener('change', () => {
@@ -18,12 +19,25 @@ trekSelect.addEventListener('change', () => {
 CalculateTrekPrice();
 
 function CalculateTrekPrice () {
-  trekPrice.textContent =
+  trekPriceFirst.textContent =
     '₹ ' +
     (
-      trekSelect[trekSelect.selectedIndex].getAttribute('per-person') *
+      trekSelect[trekSelect.selectedIndex].getAttribute('per-person-first') *
       trekPersons.value
     ).toLocaleString(undefined, {
       minimumFractionDigits: 0
-    });
+    }) +
+    ` (${trekSelect[trekSelect.selectedIndex].getAttribute('duration-first')})`;
+
+  trekPriceSecond.textContent =
+    '₹ ' +
+    (
+      trekSelect[trekSelect.selectedIndex].getAttribute('per-person-second') *
+      trekPersons.value
+    ).toLocaleString(undefined, {
+      minimumFractionDigits: 0
+    }) +
+    ` (${trekSelect[trekSelect.selectedIndex].getAttribute(
+      'duration-second'
+    )})`;
 }
